9e526d5580a939436242188382b79521f96d651c
Purushottam Kumar <praveen99887760@gmail.com>
2026-08-01 20:59:20 +0530

    Time: 4 ms (39.38%) | Memory: 19.6 MB (36.36%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,20 @@
+class Solution:
+    def predictTheWinner(self, nums: list[int]) -> bool:
+        n = len(nums)
+        # Memoization table
+        memo = {}
+        
+        def get_max_diff(i: int, j: int) -> int:
+            if i == j:
+                return nums[i]
+            if (i, j) in memo:
+                return memo[(i, j)]
+            
+            # Choose left element or right element
+            pick_left = nums[i] - get_max_diff(i + 1, j)
+            pick_right = nums[j] - get_max_diff(i, j - 1)
+            
+            memo[(i, j)] = max(pick_left, pick_right)
+            return memo[(i, j)]
+        
+        return get_max_diff(0, n - 1) >= 0
