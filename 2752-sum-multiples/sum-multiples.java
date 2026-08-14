39cd1418b6435c08aee301a89fcaeb3afb5a711c
Purushottam Kumar <praveen99887760@gmail.com>
2026-08-14 20:23:31 +0530

    Time: 3 ms (94.81%) | Memory: 42.6 MB (61.94%) - LeetSync

---
+++ 2752-sum-multiples/sum-multiples.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int totalSum = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            // Check if the number is divisible by 3, 5, or 7
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                totalSum += i;
+            }
+        }
+        
+        return totalSum;
+    }
+}
