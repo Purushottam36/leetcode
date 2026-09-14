fd58a3f74768ae4e73767ef69e20e7e4ad89b9c5
Purushottam Kumar <praveen99887760@gmail.com>
2026-09-14 08:47:09 +0530

    [3986] [Time Beats: 65.94%] [Memory Beats: 99.00%] - LeetPush

---
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-Number-of-Elapsed-Seconds-Between-Two-Times.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int secondsBetweenTimes(String startTime, String endTime) {
+        return convertToSeconds(endTime) - convertToSeconds(startTime);
+    }
+
+    private int convertToSeconds(String time) {
+        // Parse segments directly by index to avoid regex overhead from split()
+        int hours = Integer.parseInt(time.substring(0, 2));
+        int minutes = Integer.parseInt(time.substring(3, 5));
+        int seconds = Integer.parseInt(time.substring(6, 8));
+        
+        // Convert to absolute seconds since midnight
+        return (hours * 3600) + (minutes * 60) + seconds;
+    }
+}
