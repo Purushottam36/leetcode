60b2a2ff2d83c965e5a1d69a7579b09999873fa9
Purushottam Kumar <praveen99887760@gmail.com>
2026-09-24 21:28:52 +0530

    [3550] [Time Beats: 99.69%] [Memory Beats: 24.38%] - LeetPush

---
+++ DCP-09-26/3550-smallest-index-with-digit-sum-equal-to-index/3550-Smallest-Index-With-Digit-Sum-Equal-to-Index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int smallestIndex(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (getDigitSum(nums[i]) == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    
+    // Helper method to calculate the sum of digits
+    private int getDigitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+}
